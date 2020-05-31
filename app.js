06572c4f93c8c0233435b2bf17ee2369971970ad
nobobobo <ianhsu1221@gmail.com>
2020-05-30 17:29:15 -0700

    comment on app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// construct propmt questions for manager 
 const managerPrompt = [
     {
         type: "input",
@@ -30,6 +31,7 @@ const managerPrompt = [
     }
 ];
 
+// construct propmt questions for engineer
 const engineerPrompt = [
     {
         type: "input",
@@ -50,6 +52,7 @@ const engineerPrompt = [
     }
 ]
 
+// construct propmt questions for intern
 const internPrompt = [
     {
         type: "input",
@@ -70,6 +73,7 @@ const internPrompt = [
     }
 ]
 
+// construct propmt question to get next type of employee to add.
 const employeeTypePrompt = [
     {
         type: "list",
@@ -79,15 +83,23 @@ const employeeTypePrompt = [
     }
 ]
 
+// main function
 const main = async () => {
+    // initialize employees list. There will be employee class elements to be pushed into it.
     const employees =[];
 
+    // get information of manager, create a new manager class and push to employee list
     const {name, id, email, officeNum} = await inquirer.prompt(managerPrompt);
     employees.push(new Manager(name, id, email, officeNum));
+
+    // initialize isDone boolean in order to repeat inquirer
     let isDone = false;
 
     while (!isDone) {
+        // get employee type from prompt
         const { employeeType } = await inquirer.prompt(employeeTypePrompt);
+
+        // execute another prompt based on the input of the type
         if (employeeType === 'Engineer') {
             await inquirer.prompt(engineerPrompt).then(res =>{
                 employees.push(new Engineer(res.name, res.id, res.email, res.github));
@@ -96,11 +108,13 @@ const main = async () => {
             await inquirer.prompt(internPrompt).then(res =>{
                 employees.push(new Intern(res.name, res.id, res.email, res.school));
             });
+        // if the user chose not adding more, setting isDone to true, which will quit the loop.
         } else {
             isDone = true;
         }
     }
 
+    // render and write file based on the employee list. 
     fs.writeFile(outputPath, render(employees), err => {
         if (err) return console.log(err);
         console.log("A new html file is generated!");
